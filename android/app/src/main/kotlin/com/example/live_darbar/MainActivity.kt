0ac5f94942161e9adf26706be26e8c7b493e826e
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-01-15 01:23:10 +1100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/live_darbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.live_darbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.orange.shade800,
+          title: Text("Sri Darbar Sahib Live",
+          ),
+        ),
+        body: SafeArea(
+          child: AppBody(),
+        ),
+
+
+      ),
+    );
+  }
+}
+
+class AppBody extends StatefulWidget {
+  const AppBody({Key? key}) : super(key: key);
+
+  @override
+  State<AppBody> createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+
+  String buttonText = "Play";
+  bool isPlaying = false;
+  final liveKirtan = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.orange.shade800,
+        ),
+        onPressed: (){
+
+          setState( () {
+            if (isPlaying){
+              buttonText = "Play";
+              isPlaying = false;
+              liveKirtan.stop();
+
+            }
+            else{
+              buttonText = "Pause";
+              isPlaying = true;
+              liveKirtan.play(UrlSource("https://live.sgpc.net:8443/;nocache=889869audio_file.mp3"));
+
+            }
+
+
+          });
+          print("is playing: $isPlaying");
+        },
+        child: Text(buttonText),
+      ),
+    );
+  }
+}
