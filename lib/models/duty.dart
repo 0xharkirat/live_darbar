8f3777372cac1849e8986fd4794b731316a1b948
sandhukirat23 <sandhukirat23@gmail.com>
2023-05-07 03:53:43 +1000

    test commit

---
+++ lib/models/duty.dart
@@ -0,0 +1,7 @@
+class Duty {
+  const Duty(
+      {required this.ragi, required this.startTime, required this.endTime});
+  final String ragi;
+  final String startTime;
+  final String endTime;
+}
