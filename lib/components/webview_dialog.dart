1fe40f83679b9c722ef65decc912cd473e1ba1ff
sandhukirat23 <sandhukirat23@gmail.com>
2023-05-09 15:58:59 +1000

    change to ads, webview to dialog and minor bugs

--- lib/components/webview_dialog.dart
+++ lib/components/webview_dialog.dart
@@ -1,98 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:live_darbar/utils/ad_state.dart';
-import 'package:provider/provider.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewApp extends StatefulWidget {
+class WebViewApp extends StatelessWidget {
   const WebViewApp({super.key, required this.url});
-
   final String url;
-  
-
-  @override
-  State<WebViewApp> createState() => _WebViewAppState();
-}
-
-class _WebViewAppState extends State<WebViewApp> {
-  BannerAd? banner;
-  late final WebViewController controller;
-  var loadingPercentage = 0;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final adState = Provider.of<AdState>(context);
-    adState.initialization.then((status) {
-      setState(() {
-        banner = BannerAd(
-            size: AdSize.banner,
-            adUnitId: adState.bannerAdUnitId,
-            listener: adState.bannerAdListener,
-            request: const AdRequest())
-          ..load();
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..setNavigationDelegate(NavigationDelegate(onPageStarted: (url) {
-        setState(() {
-          loadingPercentage = 0;
-        });
-      }, onProgress: (progress) {
-        setState(() {
-          loadingPercentage = progress;
-        });
-      }, onPageFinished: (url) {
-        setState(() {
-          loadingPercentage = 100;
-        });
-      }))
-      ..loadRequest(
-        Uri.parse(widget.url),
-      )
-      ..setJavaScriptMode(JavaScriptMode.unrestricted);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.clearCache();
-    banner?.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Stack(
-              children: [
-                WebViewWidget(
-                  controller: controller,
-                ),
-                if (loadingPercentage < 100)
-                  LinearProgressIndicator(
-                    value: loadingPercentage / 100.0,
-                  ),
-              ],
-            ),
+    return Dialog(
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        backgroundColor: const Color(0xFF040508).withOpacity(0.80),
+        insetPadding: const EdgeInsets.all(8),
+        child: InteractiveViewer(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              const Center(child: CircularProgressIndicator(color: Color(0xFFD6DCE6),)),
+              CachedNetworkImage(imageUrl: url,)
+            ],
+            
+            
           ),
-          if (banner != null)
-                SizedBox(
-                  height: 50,
-                  child: AdWidget(ad: banner!),
-                )
-
-                
-                
-        ],
-      ),
-    );
+        ));
   }
 }
