02f6e45bc8a84e3580c2ae5d752988bd0fd0d225
sandhukirat23 <sandhukirat23@gmail.com>
2024-12-26 18:08:54 +1100

    fix: add shortcuts.xml

--- lib/src/views/screens/home_screen.dart
+++ lib/src/views/screens/home_screen.dart
@@ -36,164 +36,172 @@ class HomeScreen extends ConsumerWidget {
               ),
             ),
           ),
-          child: Scaffold(
-            appBar: AppBar(
-              leading: IconButton(
-                tooltip: AppLocalizations.of(context)!.language_tooltip,
-                onPressed: (){
-                ref.read(localeController.notifier).toggleLocale();
-              }, icon: const Icon(LucideIcons.languages)),
-              title:  Text(
-                AppLocalizations.of(context)!.app_title,
-              ),
-              centerTitle: true,
-              backgroundColor: ShadTheme.of(context).colorScheme.accent,
-              actions: [
-                const ThemeSwitchWidget(),
-                // if web or wasm, show this buttn
-                if (kIsWeb || kIsWasm) const DownloadButtonWidget(),
-                
-                IconButton(
-                    tooltip: AppLocalizations.of(context)!.refresh_tooltip,
+          child: SafeArea(
+            top: false,
+            child: Scaffold(
+              appBar: AppBar(
+                leading: IconButton(
+                    tooltip: AppLocalizations.of(context)!.language_tooltip,
                     onPressed: () {
-                      ref.read(audioController).stop();
+                      ref.read(localeController.notifier).toggleLocale();
                     },
-                    icon: const Icon(LucideIcons.rotateCcw)),
-                IconButton(
-                  tooltip: AppLocalizations.of(context)!.about_tooltip,
-                  icon: const Icon(LucideIcons.info),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return Center(
-                          child: ConstrainedBox(
-                            constraints:
-                                const BoxConstraints(maxWidth: maxWidth),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                border: Border.symmetric(
-                                  vertical: BorderSide(
-                                    color: ShadTheme.of(context)
-                                        .colorScheme
-                                        .border,
+                    icon: const Icon(LucideIcons.languages)),
+                title: Text(
+                  AppLocalizations.of(context)!.app_title,
+                ),
+                centerTitle: true,
+                backgroundColor: ShadTheme.of(context).colorScheme.accent,
+                actions: [
+                  const ThemeSwitchWidget(),
+                  // if web or wasm, show this buttn
+                  if (kIsWeb || kIsWasm) const DownloadButtonWidget(),
+
+                  IconButton(
+                      tooltip: AppLocalizations.of(context)!.refresh_tooltip,
+                      onPressed: () {
+                        ref.read(audioController).stop();
+                      },
+                      icon: const Icon(LucideIcons.rotateCcw)),
+                  IconButton(
+                    tooltip: AppLocalizations.of(context)!.about_tooltip,
+                    icon: const Icon(LucideIcons.info),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Center(
+                            child: ConstrainedBox(
+                              constraints:
+                                  const BoxConstraints(maxWidth: maxWidth),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.symmetric(
+                                    vertical: BorderSide(
+                                      color: ShadTheme.of(context)
+                                          .colorScheme
+                                          .border,
+                                    ),
                                   ),
                                 ),
+                                child: const InfoDialogWidget(),
                               ),
-                              child: const InfoDialogWidget(),
                             ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ],
-            ),
-            body: Stack(
-              children: [
-                SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 20,
-                      left: 20,
-                      right: 20,
-                      bottom: 56,
-                    ),
-                    child: Column(
-                      children: [
-                        AudioTileWidget(
-                          onTap: () {
-                            ref.read(audioController).play(0);
-                          },
-                          color: kFirstColor,
-                          id: 0,
-                          text: AppLocalizations.of(context)!.live_kirtan,
-                          imageUrl: 'assets/images/0.jpg',
-                          style: ShadTheme.of(context).textTheme.h3.copyWith(
-                                color: ShadTheme.of(context).colorScheme.accent,
-                                fontWeight: FontWeight.bold,
-                              ),
-                        ),
-                        const SizedBox(height: 20),
-                        AudioTileWidget(
-                          onTap: () {
-                            ref.read(audioController).play(1);
-                          },
-                          color: kSecondColor,
-                          id: 1,
-                          text: AppLocalizations.of(context)!.mukhwak,
-                          imageUrl: "assets/images/1.jpg",
-                          style: ShadTheme.of(context).textTheme.h3.copyWith(
-                              color:
-                                  ShadTheme.of(context).colorScheme.foreground,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 20),
-                        AudioTileWidget(
-                          onTap: () {
-                            ref.read(audioController).play(2);
-                          },
-                          color: kThirdColor,
-                          id: 2,
-                          text: AppLocalizations.of(context)!.mukhwak_katha,
-                          imageUrl: "assets/images/2.jpg",
-                          style: ShadTheme.of(context).textTheme.h3.copyWith(
-                              color:
-                                  ShadTheme.of(context).colorScheme.foreground,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 20),
-                      ],
-                    ),
+                          );
+                        },
+                      );
+                    },
                   ),
-                ),
-
-                // This is player controls
-                // I want to apply backdrop filter here such that when scrolling, anything which gets behind this player controls should be blurred
-                Positioned(
-                  bottom: 0,
-                  left: 12, // Added padding for left
-                  right: 12,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: Container(
-                        height: 56,
-                        width: size.width > maxWidth ? maxWidth : size.width,
-                        decoration: BoxDecoration(
-                          color: ShadTheme.of(context)
-                              .colorScheme
-                              .accent
-                              .withOpacity(0.8),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Stack(
-                          children: [
-                            const Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                PlayerDataWidget(),
-                                PlayPauseButtonWidget(),
-                              ],
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              left: 56,
-                              right: 12,
-                              child: ProgressBarWidget(
-                                  width: size.width > maxWidth
-                                      ? maxWidth
-                                      : size.width),
-                            ),
-                          ],
-                        ),
+                ],
+              ),
+              body: Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 20,
+                        left: 20,
+                        right: 20,
+                        bottom: 56,
+                      ),
+                      child: Column(
+                        children: [
+                          AudioTileWidget(
+                            onTap: () {
+                              ref.read(audioController).play(0);
+                            },
+                            color: kFirstColor,
+                            id: 0,
+                            text: AppLocalizations.of(context)!.live_kirtan,
+                            imageUrl: 'assets/images/0.jpg',
+                            style: ShadTheme.of(context).textTheme.h3.copyWith(
+                                  color:
+                                      ShadTheme.of(context).colorScheme.accent,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
+                          const SizedBox(height: 20),
+                          AudioTileWidget(
+                            onTap: () {
+                              ref.read(audioController).play(1);
+                            },
+                            color: kSecondColor,
+                            id: 1,
+                            text: AppLocalizations.of(context)!.mukhwak,
+                            imageUrl: "assets/images/1.jpg",
+                            style: ShadTheme.of(context).textTheme.h3.copyWith(
+                                color: ShadTheme.of(context)
+                                    .colorScheme
+                                    .foreground,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 20),
+                          AudioTileWidget(
+                            onTap: () {
+                              ref.read(audioController).play(2);
+                            },
+                            color: kThirdColor,
+                            id: 2,
+                            text: AppLocalizations.of(context)!.mukhwak_katha,
+                            imageUrl: "assets/images/2.jpg",
+                            style: ShadTheme.of(context).textTheme.h3.copyWith(
+                                color: ShadTheme.of(context)
+                                    .colorScheme
+                                    .foreground,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
                       ),
                     ),
                   ),
-                )
-              ],
+
+                  // This is player controls
+                  // I want to apply backdrop filter here such that when scrolling, anything which gets behind this player controls should be blurred
+                  Positioned(
+                    bottom: 0,
+                    left: 12, // Added padding for left
+                    right: 12,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          height: 56,
+                          width: size.width > maxWidth ? maxWidth : size.width,
+                          decoration: BoxDecoration(
+                            color: ShadTheme.of(context)
+                                .colorScheme
+                                .accent
+                                .withOpacity(0.8),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Stack(
+                            children: [
+                              const Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  PlayerDataWidget(),
+                                  PlayPauseButtonWidget(),
+                                ],
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                left: 56,
+                                right: 12,
+                                child: ProgressBarWidget(
+                                    width: size.width > maxWidth
+                                        ? maxWidth
+                                        : size.width),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
